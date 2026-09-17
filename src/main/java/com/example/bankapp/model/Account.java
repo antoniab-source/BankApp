8006017ef03da13bf5dd906455566836064c7017
Antonia B <antoniaambrielle@ymail.com>
2026-09-17 03:05:29 -0400

    step 2 add database

--- src/main/java/com/example/bankapp/model/Account.java
+++ src/main/java/com/example/bankapp/model/Account.java
@@ -1,12 +1,25 @@
 package com.example.bankapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int accountID;
+
     private String accountNumber;
     private BigDecimal balance;
     private String accountType;
+
+    public Account() {
+    }
 
     public Account(int accountID, String accountNumber, BigDecimal balance, String accountType) {
         this.accountID = accountID;
@@ -14,28 +27,36 @@ public class Account {
         this.balance = balance;
         this.accountType = accountType;
     }
-        public int getAccountID() {
-            return accountID;
-        }
-        public String getAccountNumber() {
-            return accountNumber;
-        }
-        public BigDecimal getBalance() {
-            return balance;
-        }
-        public String getAccountType() {
-            return accountType;
-        }
-         public void setAccountID(int accountID) {
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountID(int accountID) {
         this.accountID = accountID;
     }
-        public void setAccountNumber(String accountNumber) {
+
+    public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
-        public void setBalance(BigDecimal balance) {
-            this.balance = balance;
-        }
-        public void setAccountType(String accountType) {
-            this.accountType = accountType;
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
     }
 }
